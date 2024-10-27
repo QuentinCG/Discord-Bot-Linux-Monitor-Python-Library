862fd8f5b8dd8711cf553cd8e9c3bc5c8a093eb4
Quentin CG <quentin@comte-gaz.com>
2024-10-27 16:26:59 +0100

    1.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with io.open(file='README.md', mode='r', encoding='utf-8') as readme_file:
 
 setup(
     name="DiscordBotLinuxMonitor",
-    version="1.4.3",
+    version="1.5.0",
     description="From discord channels: Get information and warning status of Linux server like service, port, ping, ssl certificate, disk/folder/cpu/ram/swap usage, ip connection, ... (Python and shell library, Linux ONLY)",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -36,7 +36,7 @@ setup(
     keywords='discord bot warning info linux monitor monitoring server service port ping ssl certificate disk folder cpu ram swap usage ip connection',
     platforms='Linux',
     install_requires=[
-        "linuxmonitor~=1.4.0",  # follows the {MAJOR}.{MINOR}.x version range (because LinuxMonitor and DiscordBotLinuxMonitor are tightly coupled and should have same major and minor version)
+        "linuxmonitor~=1.5.0",  # follows the {MAJOR}.{MINOR}.x version range (because LinuxMonitor and DiscordBotLinuxMonitor are tightly coupled and should have same major and minor version)
         "discord.py",
         "typing",
         "asyncio",
